a31d8141a49c5786bdeda4e90d7502cd7993492c
Elias Ojala <git@eliasojala.me>
2018-08-07 14:36:41 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install ffmpeg v4l-utils -y
+sudo apt install libmariadbclient18 libpq5 -y
+sudo wget https://github.com/Motion-Project/motion/releases/download/release-4.1.1/pi_stretch_motion_4.1.1-1_armhf.deb
+sudo dpkg -i pi_stretch_motion_4.1.1-1_armhf.deb
+sudo apt-get install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
+sudo pip install motioneye
+sudo mkdir -p /etc/motioneye
+sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
+sudo mkdir -p /var/lib/motioneye
+sudo cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+sudo systemctl daemon-reload
+sudo systemctl enable motioneye
+sudo systemctl start motioneye
